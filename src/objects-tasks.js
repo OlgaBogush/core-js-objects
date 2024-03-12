d5c6f31862222aeac6ca8477196573e10ffc2b5a
OlgaBogush <bohush.olga@gmail.com>
2024-03-13 00:15:14 +0300

    add: resolve for removeProperties

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -61,8 +61,13 @@ function mergeObjects(objects) {
  *    removeProperties({name: 'John', age: 30, city: 'New York'}, 'age') => {name: 'John', city: 'New York'}
  *
  */
-function removeProperties(/* obj, keys */) {
-  throw new Error('Not implemented');
+function removeProperties(obj, keys) {
+  const newObj = {};
+  Object.assign(newObj, obj);
+  keys.forEach((item) => {
+    delete newObj[item];
+  });
+  return newObj;
 }
 
 /**
