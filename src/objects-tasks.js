c376be13a91e806b31307381948f3886552f53d1
OlgaBogush <bohush.olga@gmail.com>
2024-03-12 23:59:24 +0300

    add resolve for mergeObjects

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -34,8 +34,18 @@ function shallowCopy(obj) {
  *    mergeObjects([{a: 1, b: 2}, {b: 3, c: 5}]) => {a: 1, b: 5, c: 5}
  *    mergeObjects([]) => {}
  */
-function mergeObjects(/* objects */) {
-  throw new Error('Not implemented');
+function mergeObjects(objects) {
+  const obj = {};
+  objects.forEach((item) =>
+    Object.entries(item).forEach(([key, value]) => {
+      if (obj[key]) {
+        obj[key] += value;
+      } else {
+        obj[key] = value;
+      }
+    })
+  );
+  return obj;
 }
 
 /**
